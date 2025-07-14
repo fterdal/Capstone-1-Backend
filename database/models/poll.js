4d92ca70ee8e11d25561342ca2aad07b5c437f08
rend1027 <rend1027@yahoo.com>
2025-07-13 23:23:09 -0400

    fix-fixed model attributes

--- database/models/poll.js
+++ database/models/poll.js
@@ -5,32 +5,41 @@ const db = require('../db');
 
 const Poll = db.define("poll", {
     title: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     description: {
-        type: DataTypes.TEXT
+        type: DataTypes.TEXT,
+        allowNull: true,
     },
     participants: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
     },
     status: {
-        type: DataTypes.STRING // draft, published , ended
+        type: DataTypes.ENUM, // draft, published , ended
+        allowNull: false,
     },
     deadline: {
-        type: DataTypes.TIME
+        type: DataTypes.TIMESTAMP,
+        allowNull: false,
     },
     authRequired: {
         type: DataTypes.BOOLEAN, // allow only user votes if true
-        default: false
+        default: false,
+        allowNull: false,
     },
     isDisabled: {
         type: DataTypes.BOOLEAN, // if true poll is disabled by admin
-        default: false
+        default: false,
+        allowNull: false,
     },
     restricted: {
         type: DataTypes.BOOLEAN, // only specic users can parcipate if true
-        default: false
-    }
+        default: false,
+        allowNull: false,
+    },
+    timestamps: true
 });
 
 module.exports = Poll;

ca01929446f9002d408692828d692688d0663d5c
rend1027 <rend1027@yahoo.com>
2025-07-13 21:46:10 -0400

    feat-define Poll model

---
+++ database/models/poll.js
@@ -0,0 +1,36 @@
+const {DataTypes} = require('sequelize');
+const db = require('./db');
+
+// define the Poll model
+
+const Poll = db.define("poll", {
+    title: {
+        type: DataTypes.STRING
+    },
+    description: {
+        type: DataTypes.TEXT
+    },
+    participants: {
+        type: DataTypes.INTEGER
+    },
+    status: {
+        type: DataTypes.STRING // draft, published , ended
+    },
+    deadline: {
+        type: DataTypes.TIME
+    },
+    authRequired: {
+        type: DataTypes.BOOLEAN, // allow only user votes if true
+        default: false
+    },
+    isDisabled: {
+        type: DataTypes.BOOLEAN, // if true poll is disabled by admin
+        default: false
+    },
+    restricted: {
+        type: DataTypes.BOOLEAN, // only specic users can parcipate if true
+        default: false
+    }
+});
+
+module.exports = Poll;

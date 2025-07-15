c4c446df5a8ad7fd9534aa796d40fb45ce780d2d
tabularasae <tabularasae@icloud.com>
2025-07-15 12:27:37 -0400

    Defined the API routes for the poll options

--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const usersRouter = require("./users");
 const pollsRouter = require("./polls");
+const pollOptionsRouter = require("./pollOptions");
 
 router.use("/users", usersRouter);
-router.use("/polls", pollsRouter); 
+router.use("/polls", pollsRouter);
+router.use("/pollOptions", pollOptionsRouter);
 
 module.exports = router;

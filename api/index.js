798b846e7cdeee404f9a3bff4d397014172554ea
Florencio Rendon <rend1027@yahoo.com>
2025-07-15 15:54:11 -0400

    feat(api)-poll router

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const pollRouter = express.Router();
 const testDbRouter = require("./test-db");
 
 router.use("/test-db", testDbRouter);
+router.use("/poll", pollRouter)
 
 module.exports = router;

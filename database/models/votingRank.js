ea5cab20359f7e762f5b19e52cd74e18eb9ad7c6
Florencio Rendon <rend1027@yahoo.com>
2025-07-15 12:28:09 -0400

    feat- added attributes to VotingRank

--- database/models/votingRank.js
+++ database/models/votingRank.js
@@ -4,6 +4,14 @@ const db = require("../db");
 // define the votingRank model
 
 const VotingRank = db.define("votingRank", {
+    voteId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    pollOptionId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
     rank: {
         type: DataTypes.INTEGER,
         allowNull: false,

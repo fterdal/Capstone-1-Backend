f25f397cd394a207b29115b0b12df34627f88b37
Florencio Rendon <rend1027@yahoo.com>
2025-07-15 00:56:46 -0400

    feat- association

--- database/index.js
+++ database/index.js
@@ -3,16 +3,20 @@ const User = require("./models/user");
 const Poll = require("./models/poll");
 const PollOption = require("./models/pollOption");
 const VotingRank = require("./models/votingRank");
+const Vote = require("./models/vote");
+
 //One to many - user has many polls
 User.hasMany(Poll, {
   foreignKey: 'userId',
   // onDelete: 'CASCADE' deletes poll is user is deleted
 });
 
-// One to one - Each Poll belongs to one user
+
+// many to one - Each Poll belongs to one user
 Poll.belongsTo(User, {
   foreignKey: 'userId',
 });
+
 
 // One to many - one Poll has many options
 Poll.hasMany(PollOption, {
@@ -20,14 +24,60 @@ Poll.hasMany(PollOption, {
   onDelete: "CASCADE", // delete poll_options if poll is deleted
 });
 
-// one to one - Each pollOption belongs to one Poll
+
+// many to one - Each pollOption belongs to one Poll
 PollOption.belongsTo(Poll, {
   foreignKey: "pollId"
+});
+
+
+// one to many- each user can submit many votes
+User.hasMany(Vote, {
+  foreignKey: "userId",
 })
+
+
+// one to one- Each vote(ballot) belongs to a user
+Vote.belongsTo(User, {
+  foreignKey: "userId",
+})
+
+
+// many to one - Each vote(ballot) belongs to a poll
+Vote.belongsTo(Poll, {
+  foreignKey: "pollId"
+})
+
+
+// one to many- each vote(ballot) can have many ranked options
+Vote.hasMany(VotingRank, {
+  foreignKey: "voteRankId",
+})
+
+
+//one to many- A vote(ballot) can have many ranked options
+VotingRank.belongsTo(Vote, {
+  foreignKey: 'voteId',
+});
+
+
+// many to one - each rank entry belongs to one vote(ballot)
+VotingRank.belongsTo(Vote, {
+  foreignKey: "voteId",
+})
+
+
+// many to one- Each voteRank belongs to one Polloption
+VotingRank.belongsTo(PollOption, {
+  foreignKey: 'pollOptionId',
+})
+
+
 module.exports = {
   db,
   User,
   Poll,
   PollOption,
+  Vote,
   VotingRank,
 };

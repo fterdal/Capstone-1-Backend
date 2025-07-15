d6dcc18ebedcd641716d0edcfd2c77f5a3b46f05
Florencio Rendon <rend1027@yahoo.com>
2025-07-15 14:35:30 -0400

    feat- defined associations for restricted

--- database/index.js
+++ database/index.js
@@ -6,6 +6,8 @@ const VotingRank = require("./models/votingRank");
 const Vote = require("./models/vote");
 const RestrictedPollAccess = require('./models/restrictedPollAccess')
 
+//----- User Model--------
+
 //One to many - user has many polls
 User.hasMany(Poll, {
   foreignKey: 'userId',
@@ -13,11 +15,17 @@ User.hasMany(Poll, {
 });
 
 
+
+//----- Poll Model--------
+
 // many to one - Each Poll belongs to one user
 Poll.belongsTo(User, {
   foreignKey: 'userId',
 });
 
+
+
+//----- PollOption Model--------
 
 // One to many - one Poll has many options
 Poll.hasMany(PollOption, {
@@ -25,30 +33,33 @@ Poll.hasMany(PollOption, {
   onDelete: "CASCADE", // delete poll_options if poll is deleted
 });
 
-
 // many to one - Each pollOption belongs to one Poll
 PollOption.belongsTo(Poll, {
   foreignKey: "pollId"
 });
 
 
+
+//----- Vote Model--------
+
 // one to many- each user can submit many votes
 User.hasMany(Vote, {
   foreignKey: "userId",
 })
-
 
 // one to one- Each vote(ballot) belongs to a user
 Vote.belongsTo(User, {
   foreignKey: "userId",
 })
 
-
 // many to one - Each vote(ballot) belongs to a poll
 Vote.belongsTo(Poll, {
   foreignKey: "pollId"
 })
 
+
+
+//----- Voting Rank Model--------
 
 // one to many- each vote(ballot) can have many ranked options
 Vote.hasMany(VotingRank, {
@@ -66,6 +77,27 @@ VotingRank.belongsTo(Vote, {
 VotingRank.belongsTo(PollOption, {
   foreignKey: 'pollOptionId',
 })
+
+
+
+//----- Restricted Access Model--------
+
+//A user can be restricted to many polls
+User.belongsToMany(Poll, {
+  through: RestrictedPollAccess,
+  as: "restrictedPolls",
+  foreignKey: "userId"
+})
+
+// A poll can belong to many users through Restrictred access
+Poll.belongsToMany(User, {
+  through: RestrictedPollAccess,
+  as: "restictedUsers",
+  foreignKey: "pollId",
+})
+
+// 
+
 
 
 module.exports = {

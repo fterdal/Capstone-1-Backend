c4c1cc5d70d28982e18b249b9ba3f26cc8b81f4d
Dagostocsc <Deborahagosto19@gmail.com>
2025-07-15 11:47:52 -0400

    created a Roles table

--- database/roles.js
+++ database/roles.js
@@ -2,3 +2,24 @@ const { DataTypes } = require("sequelize");
 const db = require("./db");
 const bcrypt = require("bcrypt");
 
+const Roles = db.define ("roles", 
+    {
+        id:
+        {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name:
+        {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description:
+        {
+            type: DataTypes.TEXT,
+            allowNull: true
+        }
+    });
+
+    module.exports = Roles;

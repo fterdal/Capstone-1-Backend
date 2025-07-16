39e99ffb3515e481a41dbf3034f2ab76da13b4fa
TJordan77 <tvon777@gmail.com>
2025-07-16 09:04:32 -0400

    Update poll.js typo in import

--- api/poll.js
+++ api/poll.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Polls, PollOptions } = require("../database");
+const { Polls, PollOption } = require("../database");
 
 router.get("/", async (req, res) => {
   try {

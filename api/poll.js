64707accf26eb3da1515da6886b3b93c14331f0f
Florencio Rendon <rend1027@yahoo.com>
2025-07-16 10:53:45 -0400

    draft poll creation

---
+++ api/poll.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const { Poll } = require("../database");
+
+// Create poll 
+router.post("/", async (req, res) => {
+    try {
+        const poll = req.body;
+
+        if (!poll) {
+            return res.status(400).json({ error: "Make sure to meet all constraints" });
+        }
+
+        const newPoll = await Poll.create(poll);
+        res.status(201).json(newPoll)
+    } catch (error) {
+        res.status(500).json({
+            error: "Failed to create poll",
+            message: "Check that api end points match"
+        })
+    }
+})

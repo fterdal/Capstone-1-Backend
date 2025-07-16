56199815252a31d92dcdbe3bc873576e43ad6293
Dagostocsc <Deborahagosto19@gmail.com>
2025-07-16 14:31:17 -0400

    forgot to do exports and needed to require ballot and vote at line 5 and 6

--- database/index.js
+++ database/index.js
@@ -2,9 +2,12 @@ const db = require("./db");
 const User = require("./user");
 const Polls = require("./Polls");
 const PollOption = require("./poll_options");
+const Vote = require("./vote")
+const Ballot = require("ballot")
 
 Polls.belongsTo(User);
 User.hasMany(Polls);
+
 
 Polls.hasMany(PollOption);
 PollOption.belongsTo(Polls);
@@ -31,5 +34,7 @@ module.exports = {
   User,
   Polls,
   PollOption,
+  Ballot,
+  Vote,
 };
 

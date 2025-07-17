f68d3696d23e0961ebb4f75f58d63ed73fb94af5
TJordan77 <tvon777@gmail.com>
2025-07-16 21:49:49 -0400

    fixed associations bug; api call test was successful

--- database/index.js
+++ database/index.js
@@ -2,32 +2,34 @@ const db = require("./db");
 const User = require("./user");
 const Polls = require("./Polls");
 const PollOption = require("./poll_options");
-const Vote = require("./vote")
-const Ballot = require("./ballot")
+const Vote = require("./vote");
+const Ballot = require("./ballot");
 
-Polls.belongsTo(User);
-User.hasMany(Polls);
+//Bidirectional relationship logic
+// USER ↔ POLLS
+Polls.belongsTo(User, { foreignKey: "user_id" });
+User.hasMany(Polls, { foreignKey: "user_id" });
 
+// POLLS ↔ OPTIONS
+Polls.hasMany(PollOption, {
+  foreignKey: "pollId",
+  as: "options",
+  onDelete: "CASCADE",
+});
+PollOption.belongsTo(Polls, {
+  foreignKey: "pollId",
+  as: "poll",
+});
 
-Polls.hasMany(PollOption);
-PollOption.belongsTo(Polls);
-
-Ballot.hasMany(Vote);
-Vote.belongsTo(Ballot);
-
-// Polls.hasMany(PollOption, {
-//   foreignKey: "pollId",
-//   as: "options",
-//   onDelete: "CASCADE",
-// });
-
-// PollOption.belongsTo(Polls, {
-//   foreignKey: "pollId",
-//   as: "poll",
-// });
-
-// Polls.associate?.({ PollOption });
-// PollOption.associate?.({ Polls });
+// BALLOT ↔ VOTE
+Ballot.hasMany(Vote, {
+  foreignKey: "ballotId",
+  as: "votes",
+});
+Vote.belongsTo(Ballot, {
+  foreignKey: "ballotId",
+  as: "ballot",
+});
 
 module.exports = {
   db,
@@ -37,4 +39,3 @@ module.exports = {
   Ballot,
   Vote,
 };
-

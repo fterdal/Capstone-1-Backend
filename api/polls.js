837516281ee2c35aa40e6e0a82c51468251a2ec1
Florencio Rendon <rend1027@yahoo.com>
2025-07-17 02:07:32 -0400

    feat- create poll with options

--- api/polls.js
+++ api/polls.js
@@ -1,30 +1,43 @@
 const express = require("express");
 const router = express.Router();
-const { Poll } = require("../database");
+const { Poll, PollOption } = require("../database");
 const { authenticateJWT } = require("../auth")
 
 
-// Create poll 
+// Create polls
 router.post("/", authenticateJWT, async (req, res) => {
+    const userId = req.user.id
+    const { title, description, deadline, status, options = [] } = req.body;
     try {
-        const userId = req.user.id
-        const pollData = req.body;
-
-        if (!pollData) {
-            return res.status(400).json({ error: "Make sure to meet all constraints" });
-        }
 
         const newPoll = await Poll.create({
-            ...pollData,
-            userId
+            title,
+            description,
+            deadline,
+            status,
+            userId,
         });
-        res.status(201).json(newPoll)
+
+        //[opttion1, option2, option3]
+        if (options.length > 0) {
+            const formattedOptions = options.map(text => ({
+                optionText: text,
+                pollId: newPoll.id
+            }));
+
+            await PollOption.bulkCreate(formattedOptions)
+            res.status(201).json({
+                message: "Poll and options created",
+                poll: newPoll
+            });
+        }
     } catch (error) {
         res.status(500).json({
             error: "Failed to create poll",
-            message: "Check that api end points match"
+            message: "Check that API fields and data are correct"
         })
     }
 })
+
 
 module.exports = router;

687f816cc8780e95674d6e309d7838f310026313
hailia sommerville <hailiassommerville@gmail.com>
2025-07-19 17:22:00 -0400

    feat: check if user already voted

--- middleware/checkDuplicateVote.js
+++ middleware/checkDuplicateVote.js
@@ -1,5 +1,27 @@
-const {Vote} = require("../database");
+const { Vote } = require("../database");
 
-const checkDuplicateVote = async (req, res, next) => {};
+const checkDuplicateVote = async (req, res, next) => {
+  const userId = req.user.id;
+  const pollId = req.body.pollId;
+
+  try {
+    if (userId) {
+      //check if user already voted
+      const existingVote = await Vote.findOne({
+        where: {
+          userId: userId,
+          pollId: pollId,
+        },
+      });
+
+      if (existingVote) {
+        return res
+          .status(409)
+          .json({ error: "You have already voted on this poll." });
+      }
+    }
+    
+  } catch (error) {}
+};
 
 module.exports = checkDuplicateVote;

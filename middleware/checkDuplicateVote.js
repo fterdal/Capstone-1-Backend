9ad7c03f3488b934748b3100f42ffc048825310f
hailia sommerville <hailiassommerville@gmail.com>
2025-07-19 17:22:58 -0400

    feat: check if guest already voted through voterToken

--- middleware/checkDuplicateVote.js
+++ middleware/checkDuplicateVote.js
@@ -20,7 +20,21 @@ const checkDuplicateVote = async (req, res, next) => {
           .json({ error: "You have already voted on this poll." });
       }
     }
-    
+    else {
+        const guestUser = req.body.voterToken;
+        //check if guest user already voted
+        const existingGuestVote = await Vote.findOne({
+          where: {
+            voterToken: guestUser,
+            pollId: pollId,
+          },
+        });
+        if (existingGuestVote) {
+          return res
+            .status(409)
+            .json({ error: "You have already voted on this poll." });
+        }
+    }
   } catch (error) {}
 };
 

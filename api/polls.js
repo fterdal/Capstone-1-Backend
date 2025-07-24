579647e86b2f86dd289667bfd68330a6093e97df
Heavenwilson <heavenwilson210@gmail.com>
2025-07-24 12:56:35 -0400

    since the beginning of time i had to fix all the routes cuz none of them were matching and all the attributes cuz none of them were working

--- api/polls.js
+++ api/polls.js
@@ -758,7 +758,7 @@ router.get("/:pollId/results", authenticateJWT, blockIfDisabled, async (req, res
 })
 
 // admin route to fetch all polls
-router.get("/all", authenticateJWT, isAdmin, async (req, res) => {
+router.get("/admin/all", authenticateJWT, isAdmin, async (req, res) => {
   try {
     const polls = await Poll.findAll({
       include: [{ model: PollOption }],

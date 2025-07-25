cdab282a3959a02f4b79dc0be114cf63fa8e2de4
rend1027 <rend1027@yahoo.com>
2025-07-25 11:15:51 -0400

    change cookie setting again

--- auth/index.js
+++ auth/index.js
@@ -424,7 +424,7 @@ router.post("/signup/email", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     });
 

fee0813662bdd9371d7fe8631220c3b75735ef3d
rend1027 <rend1027@yahoo.com>
2025-07-25 11:06:34 -0400

    changed cookie config for prduction

--- auth/index.js
+++ auth/index.js
@@ -100,7 +100,7 @@ router.post("/auth0", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "None",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     });
 
